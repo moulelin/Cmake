5b935cc476c09617e3061b6a32977cf7edc1daab
Lin Moule <moulecs@gmail.com>
2022-11-26 21:53:28 +0100

    cmake

--- demo_git_config/include/utils.h
+++ demo_git_config/include/utils.h
@@ -5,9 +5,9 @@
 #define VERSION_MINOR 0
 #define VERSION_PATCH 1
 
-#define BUILD_TIMESTAMP "2022-11-26 21:27:11"
+#define BUILD_TIMESTAMP "2022-11-26 21:35:07"
 
 #define GIT_BRANCH "master"
-#define GIT_HASH ""
+#define GIT_HASH "0e1f110"
 
 #endif // UTILS_H_IN
